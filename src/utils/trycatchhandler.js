1114aa7614b58ac116143a8f790bbef5046dfaab
Anjali Bhargav <anjalibhargav1122@gmail.com>
2024-12-03 14:48:31 +0530

    user management work

--- src/utils/trycatchhandler.js
+++ src/utils/trycatchhandler.js
@@ -1,26 +1,24 @@
-// transactionHelper.js
 const mongoose = require('mongoose');
 
-// Yeh function transaction ko handle karega
 async function executeTransaction(callback) {
-    const session = await mongoose.startSession(); // Mongoose session start karna
-    session.startTransaction(); // Transaction ko start karte hain
+    const session = await mongoose.startSession(); 
+    session.startTransaction();
 
     try {
-        // Callback function ko pass karte hain, jisme user-specific logic hoga
+       
         const result = await callback(session);
 
-        // Agar sab kuch theek ho, toh transaction ko commit karenge
+      
         await session.commitTransaction();
         
-        return { success: true, data: result }; // Success ka result return karenge
+        return { success: true, data: result }; 
     } catch (error) {
-        // Agar error aaye, toh transaction ko rollback karenge
+      
         await session.abortTransaction();
 
-        return { success: false, error: error.message || error }; // Error ka result return karenge
+        return { success: false, error: error.message || error }; 
     } finally {
-        // Session ko close karenge
+        
         session.endSession();
     }
 }
